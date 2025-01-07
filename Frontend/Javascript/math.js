0d1069a7ec548c1bdeb18f0a4bbbaec978b46c5d
Juyoung Jeong <wdd1016@gmail.com>
2025-01-07 09:12:46 +0900

    [12.30] axios, module

---
+++ Frontend/Javascript/math.js
@@ -0,0 +1,8 @@
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+
+/*
+export default function multiply(a, b) {
+  return a * b;
+}
+*/
---
+++ Frontend/Javascript/mathUse.js
@@ -0,0 +1,10 @@
+import { add, subtract } from "./math.js";
+
+console.log(add(5, 3)); // 8
+console.log(subtract(5, 3)); // 2
+
+/*
+import multiply from './math.js';
+
+console.log(multiply(5, 3));   // 15
+*/
