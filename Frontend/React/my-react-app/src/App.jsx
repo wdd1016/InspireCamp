558d2c1a3cc07352a8c19a13b7ef3aa5b4cb1d8c
Juyoung Jeong <wdd1016@gmail.com>
2025-01-10 11:02:42 +0900

    Fix: create-react-app -> vite (12.31 - 1.6 react)

--- Frontend/React/my-react-app/src/App.jsx
+++ Frontend/React/my-react-app/src/App.jsx
@@ -80,7 +80,7 @@ function App() {
       {/* <Theme /> */}
       {/* <ContextForCounter /> */}
       {/* <ContextForDisplay /> */}
-      <CustomHook />
+      {/* <CustomHook /> */}
     </>
   );
 }
