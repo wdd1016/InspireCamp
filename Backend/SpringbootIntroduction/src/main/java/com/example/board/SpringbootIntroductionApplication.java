2401da386d20326bee5e73439a4ff15079494377
Juyoung Jeong <wdd1016@gmail.com>
2025-01-22 14:05:16 +0900

    [1.22] Spring Boot Introduction

---
+++ Backend/SpringbootIntroduction/src/main/java/com/example/board/SpringbootIntroductionApplication.java
@@ -0,0 +1,14 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 자동 설정
+@SpringBootApplication
+public class SpringbootIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIntroductionApplication.class, args);
+    }
+
+}
