06feec31fbc8cd4ad1261a0a1e80c5dc11b53515
Juyoung Jeong <wdd1016@gmail.com>
2025-01-24 16:45:51 +0900

    [1.22] Spring Boot Board Project exception handling

--- Backend/SpringbootIntroduction/src/main/java/board/SpringbootIntroductionApplication.java
+++ Backend/SpringbootIntroduction/src/main/java/board/SpringbootIntroductionApplication.java
@@ -2,14 +2,17 @@ package board;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @Slf4j
 // 자동 설정
 @SpringBootApplication
+// @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class SpringbootIntroductionApplication {
 
-    // private static Logger log = LoggerFactory.getLogger(SpringbootIntroductionApplication.class);
+    /* private static Logger log = LoggerFactory.getLogger(SpringbootIntroductionApplication.class); */
 
     public static void main(String[] args) {
         log.trace("trace");
