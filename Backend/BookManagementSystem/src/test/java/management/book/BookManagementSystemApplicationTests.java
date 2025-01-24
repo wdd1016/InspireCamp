ee2256add13ca858ea2ad2809ca77d38bd23c76a
Juyoung Jeong <wdd1016@gmail.com>
2025-01-24 17:11:25 +0900

    [1.24] Book management system environment

---
+++ Backend/BookManagementSystem/src/test/java/management/book/BookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package management.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
