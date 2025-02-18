b5dc69e50586cae084094fffbe0b741b67a89312
Juyoung Jeong <wdd1016@gmail.com>
2025-02-18 15:19:49 +0900

    [2.17] multi-locale, HATEOAS, actuator, multi-state build, spring cloud config, license service

--- SpringCloud/license-service/src/main/java/com/optimagrowth/license/model/License.java
+++ SpringCloud/license-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -1,17 +1,41 @@
 package com.optimagrowth.license.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @ToString
-public class License {
-    private int id;
+@Entity
+@Table(name = "licenses")
+public class License extends RepresentationModel<License> {
+    // private int id;
+    @Id
+    @Column(name = "license_id", nullable = false)
     private String licenseId;
+
     private String description;
+
+    @Column(name = "organization_id", nullable = false)
     private String organizationId;
+
+    @Column(name = "prodect_name", nullable = false)
     private String productName;
+
+    @Column(name = "license_type", nullable = false)
     private String licenseType;
+
+    @Column(name = "comment")
+    private String comment;
+
+    public License withComment(String comment) {
+        this.setComment(comment);
+        return this;
+    }
 }
--- SpringCloud/license-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ SpringCloud/license-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,7 +1,13 @@
 package com.optimagrowth.license.service;
 
+import java.util.Locale;
 import java.util.Random;
+import java.util.UUID;
 
+import com.optimagrowth.license.config.ServiceConf;
+import com.optimagrowth.license.repository.LicenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import com.optimagrowth.license.model.License;
@@ -11,41 +17,46 @@ import com.optimagrowth.license.model.License;
 
 @Service
 public class LicenseService {
+    @Autowired
+    MessageSource messages;
+
+    @Autowired
+    LicenseRepository licenseRepository;
+
+    @Autowired
+    ServiceConf config;
 
     public License getLicense(String licenseId, String organizationId) {
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        if (license == null) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            messages.getMessage("license.search.error.message", null, null),
+                            licenseId, organizationId));
+        }
+
+        return license.withComment(config.getProperty());
+    }
+
+    public License createLicense(License license) {
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+
+    public License updateLicense(License license) {
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+
+    public String deleteLicense(String licenseId) {
         License license = new License();
-        license.setId(new Random().nextInt(1000));
         license.setLicenseId(licenseId);
-        license.setOrganizationId(organizationId);
-        license.setDescription("Software product");
-        license.setProductName("Ostock");
-        license.setLicenseType("full");
-        return license;
-    }
+        licenseRepository.delete(license);
 
-    public String createLicense(License license, String organizationId) {
-        String responseMessage = null;
-        if (license != null) {
-            license.setOrganizationId(organizationId);
-            responseMessage = String.format("License created %s", license.toString());
-        }
-        return responseMessage;
-    }
-
-    public String updateLicense(License license, String organizationId) {
-        String responseMessage = null;
-        if (license != null) {
-            license.setOrganizationId(organizationId);
-            responseMessage = String.format("License updated %s", license.toString());
-        }
-        return responseMessage;
-    }
-
-    public String deleteLicense(String licenseId, String organizationId) {
         String responseMessage = null;
         responseMessage
-                = String.format("Deleting license with id %s for the organization %s",
-                licenseId, organizationId);
+                = String.format(messages.getMessage("license.delete.message", null, null), licenseId);
         return responseMessage;
     }
 }
