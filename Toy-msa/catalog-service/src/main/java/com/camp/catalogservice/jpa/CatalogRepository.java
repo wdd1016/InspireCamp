87339e2f2c9492aa5b9585388d2047d4edb4bb19
Juyoung Jeong <wdd1016@gmail.com>
2025-03-06 17:35:13 +0900

    [3.6] Catalog service by https://github.com/joneconsulting/toy-msa/tree/springboot3.2/catalog-service

---
+++ Toy-msa/catalog-service/src/main/java/com/camp/catalogservice/jpa/CatalogRepository.java
@@ -0,0 +1,7 @@
+package com.camp.catalogservice.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+    CatalogEntity findByProductId(String productId);
+}
---
+++ Toy-msa/catalog-service/src/main/java/com/camp/catalogservice/service/CatalogService.java
@@ -0,0 +1,7 @@
+package com.camp.catalogservice.service;
+
+import com.camp.catalogservice.jpa.CatalogEntity;
+
+public interface CatalogService {
+    Iterable<CatalogEntity> getAllCatalogs();
+}
