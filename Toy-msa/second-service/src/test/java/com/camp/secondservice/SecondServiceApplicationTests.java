7e9c1fe66dffe895bcc0fb6508356bdb541f2145
Juyoung Jeong <wdd1016@gmail.com>
2025-03-06 15:50:48 +0900

    [3.6] Second service by https://github.com/joneconsulting/toy-msa/tree/main/second-service

---
+++ Toy-msa/second-service/src/test/java/com/camp/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
