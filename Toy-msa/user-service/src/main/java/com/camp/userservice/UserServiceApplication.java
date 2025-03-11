efae0d4c3e5cb9856ac244e87c4ab5849bde8cf3
Juyoung Jeong <wdd1016@gmail.com>
2025-03-11 10:46:27 +0900

    [3.11] User-service - Connect to order-service using a rest template

--- Toy-msa/user-service/src/main/java/com/camp/userservice/UserServiceApplication.java
+++ Toy-msa/user-service/src/main/java/com/camp/userservice/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,7 +23,7 @@ public class UserServiceApplication {
     }
 
     @Bean
-//    @LoadBalanced
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         int TIMEOUT = 5000;
 
--- Toy-msa/user-service/src/main/java/com/camp/userservice/service/UserServiceImpl.java
+++ Toy-msa/user-service/src/main/java/com/camp/userservice/service/UserServiceImpl.java
@@ -12,7 +12,10 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -94,26 +98,17 @@ public class UserServiceImpl implements UserService {
 
         log.info("Before call orders microservice");
         List<ResponseOrder> ordersList = new ArrayList<>();
-        /* #1-1 Connect to order-service using a rest template */
+
+        /* #1 Connect to order-service using a rest template */
         /* @LoadBalanced 로 선언헀으면, apigateway-service로 호출 못함 */
         /* http://ORDER-SERVICE/order-service/1234-45565-34343423432/orders */
-//        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
-//        String orderUrl = String.format("http://127.0.0.1:8000/order-service/%s/orders", userId);
-//        ResponseEntity<List<ResponseOrder>> orderListResponse =
-//                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
-//                                            new ParameterizedTypeReference<List<ResponseOrder>>() {
-//                });
-//        ordersList = orderListResponse.getBody();
-        /* #1-2 Connect to catalog-service using a rest template */
-        /* http://CATALOG-SERVICE/catalog-service/catalogs */
-//        List<ResponseCatalog> catalogList = new ArrayList<>();
-//        String catalogUrl = "http://127.0.0.1:8000/catalog-service/catalogs";
-//        ResponseEntity<List<ResponseCatalog>> catalogListResponse =
-//                restTemplate.exchange(catalogUrl, HttpMethod.GET, null,
-//                                            new ParameterizedTypeReference<List<ResponseCatalog>>() {
-//                });
-//        catalogList = catalogListResponse.getBody();
-//        System.out.println(catalogList);
+        String orderUrl = String.format(Objects.requireNonNull(env.getProperty("order-service.url")), userId);
+//         String orderUrl = String.format("http://127.0.0.1:8000/order-service/%s/orders", userId);
+        ResponseEntity<List<ResponseOrder>> orderListResponse =
+                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<ResponseOrder>>() {
+                        });
+        ordersList = orderListResponse.getBody();
 
         /* Using a feign client */
         /* #2 Feign exception handling */
@@ -125,14 +120,12 @@ public class UserServiceImpl implements UserService {
             log.error(ex.getMessage());
         }
 
-        /* #3-1 ErrorDecoder */
+        /* #3 ErrorDecoder */
 //        ordersList = orderServiceClient.getOrders(userId);
 //        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitBreaker1");
 //        CircuitBreaker circuitBreaker2 = circuitBreakerFactory.create("circuitBreaker2");
 //        ordersList = circuitBreaker.run(() -> orderServiceClient.getOrders(userId),
 //                throwable -> new ArrayList<>());
-        /* #3-2 ErrorDecoder for catalog-service */
-//        List<ResponseCatalog> catalogList = catalogServiceClient.getCatalogs();
 
         userDto.setOrders(ordersList);
 
